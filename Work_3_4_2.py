e8e887cf6fe2d59f3e01156e2494b0ef4d9f4ca2
Matsu <76900768+IIMatsuII@users.noreply.github.com>
2022-01-20 16:17:41 +0500

    Add files via upload

--- Work_3_4_2.py
+++ Work_3_4_2.py
@@ -1,8 +1,9 @@
+#Открываем файл и читаем содержимое
 inf = open('D:/GitHub/dataset_3363_3.txt', 'r')
 s1 = inf.readline()
 s2 = inf.readline()
 inf.close()
-
+#открывем фаил и выполняем программу при закрытии ответ перезапишится на другой файл
 with open('D:/GitHub/dataset_3363_3.txt') as inf, open('D:/GitHub/MostPopularWord.txt','w') as out:
     maxc = 0
     s = inf.read().lower().strip().split()
