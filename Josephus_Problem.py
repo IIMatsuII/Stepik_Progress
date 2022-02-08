39edb665e5d3ade34f244cc894f41a036fded138
Matsu <76900768+IIMatsuII@users.noreply.github.com>
2022-02-08 10:41:52 +0500

    Add files via upload

--- Josephus_Problem.py
+++ Josephus_Problem.py
@@ -1,9 +1,9 @@
-n, k = int(input()), int(input())
+n, k = int(input("Введите количество людей: ")), int(input("Введите шаг работы: "))
 count = list(range(1, n+1))
 out = 0
 
 for _ in range(n-1):
     start = out % len(count)
-    out = (start + k - 1)%len(count)
+    out = (start + k - 1) % len(count)
     count.remove(count[out])
 print(*count)
